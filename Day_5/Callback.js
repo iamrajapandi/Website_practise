56f740b471e7b7f5d119aa55fc9f3e7f10914bcf
Rajapandi <rrajapandi456@gmail.com>
2024-09-24 21:30:19 +0530

    commit

--- Day_5/Callback.js
+++ Day_5/Callback.js
@@ -2,7 +2,8 @@ async function API()
 {
     try {
         const response= await fetch("https://66e527635cc7f9b6273c7026.mockapi.io/UserDetail");
-    const data=await response.json();
+        const data=await response.json();
+        console.log(data);
   
     return  data;
     } catch (error) {
@@ -13,39 +14,39 @@ async function API()
 API();
 
 
-async function APIDATA() {
-    const apidata=await API()
-    // const fl=apidata.filter((data)=>isActive)
-    const mp=apidata.map((data)=>{  console.log(data)})  
-}
-APIDATA();
+// async function APIDATA() {
+//     const apidata=await API()
+//     // const fl=apidata.filter((data)=>isActive)
+//     const mp=apidata.map((data)=>{  console.log(data)})  
+// }
+// APIDATA();
 
 
- function pro()
-{
-    return new Promise((resolve,reject)=>{
-        const x=true;
-        setTimeout(()=>{
-            if(x){
-                console.log("succes");
-            }
-            else{
+//  function pro()
+// {
+//     return new Promise((resolve,reject)=>{
+//         const x=true;
+//         setTimeout(()=>{
+//             if(x){
+//                 console.log("succes");
+//             }
+//             else{
                 
-                console.log("error");
-            }
-        },3000)
-    })
-}
-async function  datax() {
-    try {
-        const response=await pro()
-        console.log(response)
-    } catch (error) {
-        console.log(error)
+//                 console.log("error");
+//             }
+//         },3000)
+//     })
+// }
+// async function  datax() {
+//     try {
+//         const response=await pro()
+//         console.log(response)
+//     } catch (error) {
+//         console.log(error)
         
-    }
+//     }
     
-}
-// datax(); 
+// }
+// // datax(); 
 
-pro().then(data=>{console.log(data)}).catch(e=>{console.log(e)})
+// pro().then(data=>{console.log(data)}).catch(e=>{console.log(e)})
